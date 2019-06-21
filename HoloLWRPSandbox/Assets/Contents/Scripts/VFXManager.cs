f211fea441f48e8fe318f3228c9878cc32c0645b
takagi <lianosjp010@gmail.com>
2019-06-21 15:06:23 +0900

    落とし込めたサンプルを見るVFX-Samplesシーン作成

---
+++ HoloLWRPSandbox/Assets/Contents/Scripts/VFXManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] vfxPrefab;
+
+    [SerializeField]
+    GameObject mainMenu, backMenu;
+
+    GameObject nowObj;
+
+    public void SpawnVFX(int vfxnum)
+    {
+        var nowPos = Camera.main.transform.position;
+
+        nowObj = Instantiate(vfxPrefab[vfxnum], new Vector3(nowPos.x, nowPos.y, nowPos.z + 1.0f), Quaternion.identity, gameObject.transform);
+        nowObj.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        mainMenu.SetActive(false);
+        backMenu.SetActive(true);
+    }
+
+    public void BackMenu()
+    {
+        if(nowObj != null)
+        {
+            Destroy(nowObj);
+        }
+
+        backMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
